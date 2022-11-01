ca05b7fc395e91c43bcc2291f450e0b37c390ca5
Nathan B Dunkelberger <nbd2@rice.edu>
2022-11-01 16:58:46 -0500

    updating cas_friction to use appropriate casadi variables

--- src/MOE/MahiOpenExo/Dynamics/cas_Friction.cpp
+++ src/MOE/MahiOpenExo/Dynamics/cas_Friction.cpp
@@ -9,10 +9,10 @@ namespace moe {
 casadi::SX MoeDynamicModel::cas_get_Friction(){
 	casadi::SX friction = casadi::SX::zeros(4,1); 
 
-	friction(0,0) = Fk_coeff[0]*std::tanh(qd[0]*10.0) + B_coeff[0]*qd[0];
-	friction(1,0) = Fk_coeff[1]*std::tanh(qd[1]*10.0) + B_coeff[1]*qd[1];
-	friction(2,0) = Fk_coeff[2]*std::tanh(qd[2]*10.0) + B_coeff[2]*qd[2];
-	friction(3,0) = Fk_coeff[3]*std::tanh(qd[3]*10.0) + B_coeff[3]*qd[3];
+	friction(0,0) = Fk_coeff[0]*tanh(cas_qd(0)*10.0) + B_coeff[0]*cas_qd(0);
+	friction(1,0) = Fk_coeff[1]*tanh(cas_qd(1)*10.0) + B_coeff[1]*cas_qd(1);
+	friction(2,0) = Fk_coeff[2]*tanh(cas_qd(2)*10.0) + B_coeff[2]*cas_qd(2);
+	friction(3,0) = Fk_coeff[3]*tanh(cas_qd(3)*10.0) + B_coeff[3]*cas_qd(3);
 
 	return friction;
 }
